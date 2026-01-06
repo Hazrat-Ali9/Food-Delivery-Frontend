9da7f4637a09ea855f9538b96dc5e9b486eab688
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-01-06 19:19:50 +0600

    store context  🤢

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
-
+// Store context
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
