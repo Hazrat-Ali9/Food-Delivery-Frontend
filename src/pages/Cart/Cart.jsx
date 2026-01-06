ef14a2448bed456f4fca1547b5f6050529a84a54
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-01-06 19:20:20 +0600

    cart js  🤒

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import "./Cart.css";
 import { StoreContext } from "../../context/StoreContext";
 import { useNavigate } from "react-router-dom";
-
+// cart js
 const Cart = () => {
   const { cartItem, food_list, removeFromCart, getTotalCartAmount } =
     useContext(StoreContext);
